098e4d5242a30756b90a57b3dd4f0af6f0535f89
Gustavo Santos <gustavo.r.santos@outlook.com>
2021-02-19 10:45:01 -0300

    add mais funcoes de manipulacao do grafo

--- graph.h
+++ graph.h
@@ -8,13 +8,21 @@ typedef struct {
     struct Node *next;
 } Node;
 
+/**
+ * Cria um novo no, para encadear a lista de adjacencias
+ * e retorna o endereço de memoria onde esta alocado
+ */
 Node* createNode(int v) {
-    Node* newNode = malloc(sizeof(Node));
+    Node* newNode = (Node*) malloc(sizeof(Node));
     newNode->vertex = v;
     newNode ->next = NULL;
     return newNode;
 }
 
+/** 
+ * Cria o grafo, inicializando todas as listas de adjacencias 
+ * com NULL, e retorna o endereço de memoria alocado para o grafo
+ */
 Graph* createGraph(int vertices) {
     Graph* graph = (Graph*) malloc(sizeof(Graph));
     graph->numVertices = vertices;
@@ -29,15 +37,44 @@ Graph* createGraph(int vertices) {
     
 }
 
-void addEdge(Graph* graph, int v, int w) {
-    Node* newNode = createNode(w);
-    newNode->next = (Node*) graph->adjLists[v];
-
-    newNode = createNode(v);
-    newNode->next = graph->adjLists[w];
-    graph->adjLists[w] = newNode;
+/** 
+ * Procura na lista de adjacencia do vertice pivo se já existe 
+ * o outro vertice que se deseja adicionar pois caso exista
+ * nao sera possivel adicionar novamente este vertice
+ */
+int searchVertex(Graph* graph, int v, int w) {
+    Node* list = graph->adjLists[v];
+    while(list != NULL) {
+        if(list->vertex == w) {
+            return 1;
+        }
+        list = list->next;
+    }
+    return 0;
 }
 
+/**
+ * Adiciona-se uma aresta ao grafo, como o grafo é não direcional
+ * cria-se o segmento v->w e depois cria-se o w->v. Cria-se o no que apontara
+ * para NULL após isso aponta-se o proximo elemento da lista para o 
+ */
+void addEdge(Graph* graph, int v, int w) {
+    if(!searchVertex(graph, v, w)) {
+        Node* newNode = createNode(w);
+        newNode->next = graph->adjLists[v];
+        graph->adjLists[v] = newNode;
+        
+        newNode = createNode(v);
+        newNode->next = graph->adjLists[w];
+        graph->adjLists[w] = newNode;
+    } else {
+        printf("The graph already contains this edge!\n");
+    }
+}
+
+/** 
+ * Realiza-se a impressão da lista de adjacencia na tela
+ */
 void printGraph(Graph* graph) {
   int v;
   for (v = 0; v < graph->numVertices; v++) {
@@ -51,6 +88,11 @@ void printGraph(Graph* graph) {
   }
 }
 
+/**
+ * Verifica se o grafo está vazio, verificando se todas as
+ * listas de adjacencias estao vazias, caso contenha pelo menos 1
+ * não vazia significa que o grafo não é vazio
+*/
 int isGraphEmpty(Graph* graph) {
     int vertices = graph->numVertices;
 
@@ -64,6 +106,12 @@ int isGraphEmpty(Graph* graph) {
     return 1;
 }
 
+/**
+ * Realiza a liberação da memoria alocada para o grafo, para isso
+ * percorre-se todo o array de lista de adjacencias, liberando a memoria
+ * alocada para os nos das listas, após todas as listas serem liberadas
+ * finalmente libera-se o grafo
+*/
 void destroyGraph(Graph* graph) {
     int vertices = graph->numVertices;
 
--- main.c
+++ main.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 #include "graph.h"
 #include "file.h"
+/**
+ * Autor: Gustavo Rodrigues dos Santos 
+ * Matricula: 19/0014121
+ * Materia: Teoria e Aplicaçao de Grafos - Turma A
+ * Ano: 2021 - 1o semestre
+ * Data: 04/03/2021
+ */
 
-
-int main() {
+int main() {/*
     readFile();
-    /*
+    */
     Graph* graph = createGraph(4);
     addEdge(graph, 0, 1);
     addEdge(graph, 0, 2);
     addEdge(graph, 0, 3);
-    addEdge(graph, 1, 2);
+    addEdge(graph, 0, 3);
+    addEdge(graph, 1, 2);/*
     addEdge(graph, 1, 0);
     addEdge(graph, 2, 0);
-    addEdge(graph, 3, 0);
+    addEdge(graph, 3, 0);*/
 
     printGraph(graph);
 
     destroyGraph(graph);
-    printGraph(graph);*/
     return 0;
 }
